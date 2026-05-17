7b6161a6dd7c6fc9478a37d04d4b171f1bbc78b0
SASINDHAR <your-sasindhar-sasindhar9542@gmail.com>
2026-05-17 18:58:40 +0200

    Fix main calculator entry point

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 from gui.app import CalculatorApp
 
 if __name__ == "__main__":
     app = CalculatorApp()
     app.mainloop()
-=======
-
->>>>>>> cc9a05877fafeb8c9be187207128ee8fcaf62d7f
